33a0caefaa4973410a7a07f4ad49e14f6f616cd6
James Massa <james.l.massa@gmail.com>
2019-11-14 19:54:02 -0500

    feat: svg class and data loading

---
+++ js/main.js
@@ -0,0 +1,43 @@
+class Data {
+    constructor () {
+
+        queue()
+            .defer(d3.json, "data/jira-data-1.json")
+            .defer(d3.json, "data/scrum-process.json")
+            .defer(d3.json, "data/retrospective.json")
+            .await(this.wrangleData);
+    }
+
+    wrangleData(error, jiraStories, scrumText, retrospective) {
+
+        jiraStories.forEach(d => {
+            //d.field1 = +d.field1;
+        });
+
+        retrospective.forEach(d => {
+            //d.field1 = +d.field1;
+        });
+
+        this._jiraStories = jiraStories;
+        this._scrumText = scrumText;
+        this._retrospective = retrospective;
+    }
+
+    get jiraStories(){return this._jiraStories;}
+    set jiraStories(jiraStories){this._jiraStories = jiraStories;}
+
+    get scrumText(){return this._scrumText;}
+    set scrumText(scrumText){this._scrumText = scrumText;}
+
+    get retrospective(){return this._retrospective;}
+    set retrospective(retrospective){this._retrospective = retrospective;}
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const data = new Data();
+
+});
+
+
+
---
+++ js/svg.js
@@ -0,0 +1,29 @@
+class Svg {
+    constructor(container,width, height) {
+        this.margin = {top: 0, right: 0, bottom: 0, left: 0};
+        this.width = width - this.margin.left - this.margin.right;
+        this.height = height - this.margin.top - this.margin.bottom;
+
+        this.svg = d3.select(container)
+            .append("svg")
+            .attr("width", this.width)
+            .attr("height", this.height)
+            .append("g")
+            .attr("class", "main-group")
+            .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+    }
+
+    get g(){return this.svg.select('.main-group');}
+
+    get svg(){return this._svg;}
+    set svg(svg){this._svg = svg;}
+
+    get width(){return this._width;}
+    set width(width){this._width = width;}
+
+    get height(){return this._height;}
+    set height(height){this._height = height;}
+
+    get margin(){return this._margin;}
+    set margin(margin){this._margin = margin;}
+}
