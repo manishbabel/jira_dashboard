0bc7f9dbdae94be8735bbd06e02d394e1e1b47b4
James Massa <james.l.massa@gmail.com>
2019-11-16 07:18:20 -0500

    merge

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+var issueStore;
+
+queue()
+    .defer(d3.json, "data/CFX-data-scrubbed.json")
+    //TODO: add csv file for retrospective data
+    .await(dataLoaded);
+
+function dataLoaded(error, jiraData) {
+    issueStore = new IssueStore(jiraData);
+    console.log(issueStore.getSprints());
+}
+
 class Data {
     constructor () {
 
@@ -38,6 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const data = new Data();
 
 });
+
 
 
 
