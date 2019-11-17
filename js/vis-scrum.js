33b558e59ae108439c4192c99ad05dc40fa8911c
James Massa <james.l.massa@gmail.com>
2019-11-16 22:05:50 -0500

    Added scrum process diagram and main page
    :

--- js/vis-scrum.js
+++ js/vis-scrum.js
@@ -1,13 +1,41 @@
 class ScrumProcess {
     constructor(data) {
         this._data = data;
-        this.setOverviewText();
+        this.setText();
     }
 
-    setOverviewText() {
-        document.querySelector("#overview").innerText = this.data["overview"];
-    }
+    setText() {
+        document.querySelector("#content-po p").innerText =
+            this.data.roles["product-owner"];
 
+        document.querySelector("#content-sm p").innerText =
+            this.data.roles["scrum-master"];
+
+        document.querySelector("#content-tm p").innerText =
+            this.data.roles["team-member"];
+
+        document.querySelector("#content-showcase p").innerText =
+            this.data.ceremonies["showcase"];
+
+        document.querySelector("#content-retrospective p").innerText =
+            this.data.ceremonies["retrospective"];
+
+        document.querySelector("#content-daily-scrum p").innerText =
+            this.data.ceremonies["daily-scrum"];
+
+        document.querySelector("#content-product-backlog p").innerText =
+            this.data.artifacts["product-backlog"];
+
+        document.querySelector("#content-sprint-backlog p").innerText =
+            this.data.artifacts["sprint-backlog"];
+
+        document.querySelector("#content-product-increment p").innerText =
+            this.data.artifacts["product-increment"];
+
+        document.querySelector("#content-sprint p").innerText =
+            this.data.artifacts["sprint"];
+
+    }
     get data(){return this._data;}
 }
 
