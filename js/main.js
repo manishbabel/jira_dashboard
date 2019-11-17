90a69e96a89bdae2a0a376a2528889a0cbc0bb52
kdong2395 <kdong2395@gmail.com>
2019-11-17 14:44:33 -0500

    feat:(JV-29) Sketch 4

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 var issueStore;
 
+// Visualizations
+var lineVis;
+
 queue()
     .defer(d3.json, "data/CFX-data-scrubbed.json")
     //TODO: add csv file for retrospective data
+    .defer(d3.json, "data/metrics.json")
     .await(dataLoaded);
 
-function dataLoaded(error, jiraData) {
+function dataLoaded(error, jiraData, metrics) {
     issueStore = new IssueStore(jiraData);
+    lineVis = new LineChart("line-chart", metrics);
 }
